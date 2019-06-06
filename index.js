cfa3e9913c754a2b171c8dd3cb8ffa97f40c96a3
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-06 14:49:23 -0500

    cambios con syn y asynsum

--- index.js
+++ index.js
@@ -7,8 +7,11 @@
  */
 const cowsay = require("cowsay");
 const add = require("./src/addition");
-
+const sim = require("./src/intensive");
 console.log(cowsay.say({text:"hola",e:"O9",T:"u"}));
 
 add.syncSum(1,3);
 add.asyncSum(5,6);
+
+sim.simulateSync();
+sim.simulateAsync();
