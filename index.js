211f41598eddf0f57b723caf19f6d96600da60a6
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-06 14:41:31 -0500

    cambios con syn y asynsum

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
  * *************
  * 
  */
+const cowsay = require("cowsay");
+const add = require("./src/addition");
+
+console.log(cowsay.say({text:"hola",e:"O9",T:"u"}));
+
+add.syncSum(1,3);
+add.asyncSum(5,6);
