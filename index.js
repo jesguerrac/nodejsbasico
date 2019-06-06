6f5594e80c85c1d0368242db985596dc0b998756
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-06 08:46:36 -0500

    cargue

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
  * Comience aqui
  * *************
  */
+const math = require("./math");
+const greet = require("./greetings");
+const hello = require("./greetings/hello");
+
+console.log(math.add(2,4));
+console.log(math.divide(2,4));
+console.log(math.substract(2,4));
+console.log(math.add(2,4));
+
+console.log(greet.greet("eeeee"));
+
+console.log(hello.sayHello("carrr"));
