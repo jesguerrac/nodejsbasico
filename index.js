dd537f02944a2eb49be19d89f5e74bc2c2c46558
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-12 15:32:26 -0500

    vamos bien

--- index.js
+++ index.js
@@ -5,3 +5,27 @@
  * *************
  * 
  */
+const call = require("./src/call");
+
+console.log(call.sync("pedro"));
+
+    //call.withCallback("carlo",call.sync);
+
+    console.log(call.withPromises("ellooo"));
+
+    call.withPromises("ellooo")
+    .then (name => {console.log(name)})
+    console.log("hoy voy a morir");
+
+    call.great("carlos");
+    
+    call.withPromise('Alberto', 'Jaramillo')
+            .then(n => console.log(n));
+    
+    async function callwithpromise(){
+        const fullname = await call.withPromise('Alberto', 'Jaramillo')
+            console.log(fullname);
+
+    }
+
+    callwithpromise();
--- src/call.js
+++ src/call.js
@@ -1,17 +1,31 @@
 module.exports = {
     sync: function(nombre) {
-        return "Hola ${nombre}! Bienvenido a Node.js";
+       return `Hola ${nombre}! Bienvenido a Node.js`;
+        //return 'Hola'+nombre+'! Bienvenido a Node.js';
     },
     withCallback: function(nombre, cb) {
         setTimeout(()=>{
-            cb(nombre)
+            console.log(cb(nombre));
         }, 5000);
     },
-    withPromise: function(nombre) {
+    withPromises: function(nombre) {
         return new Promise((resolve, reject)=>{
             setTimeout(()=>{
                 resolve(nombre);
             }, 5000); 
+        })
+    },
+    great : function(nombreCompleto){
+        console.log(`Hola ${nombreCompleto}! Bienvenido a Node.js`);
+    },
+    withPromise : function(nombre, apellido){
+        console.log('llamado');
+        return new Promise((resolve, reject) =>{
+            console.log('Ejecutando espera..');
+            setTimeout(()=>{
+                console.log('Resolviendo, espera');
+                resolve(`${nombre} ${apellido}`);
+            },5000);
         })
     }
 }
