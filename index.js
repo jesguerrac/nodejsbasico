811890f6701640e49a88a0cc6dbb12eaa1ffa604
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-12 16:23:50 -0500

    crear un server con express

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express");
+const app = express();
+
+app.get("/app",(req,res)=>{
+    res.send("estoy en ruta app");
+});
+
+app.get("/",(req,res)=>{
+    res.send("estoy en ruta home");
+});
+
+app.get("*",(req,res)=>{
+    res.send("nose dond estoy");
+});
+
+app.listen(3000,()=>{
+    console.log('server init');
+})
