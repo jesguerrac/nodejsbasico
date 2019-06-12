72182dae83b0c8c2b2b686d14465e79d78fe6f0e
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-12 15:45:09 -0500

    crear un server

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require("http");
+const fs = require('fs');
+
+const server = http.createServer((req, res) =>{
+    fs.readFile("./resources/my_page.html",(err,data) =>{
+        if(err) { console.error(err); return;}
+        res.end(data);
+    });
+});
+console.log("inicia Server");
+server.listen(3000);
